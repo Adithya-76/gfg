4409b79487aa740d27cd2b71dfb6b2208a3fafa4
Adithya Tanam <adisuresh58@gmail.com>
2026-03-31 17:58:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, arr, k):
+        if not arr:
+            return 0
+        
+        cash = 0
+        hold = -arr[0]
+        
+        for i in range(1, len(arr)):
+            prev_cash = cash
+            cash = max(cash, hold + arr[i] - k)
+            hold = max(hold, prev_cash - arr[i])
+        
+        return cash
