0756f00fc3f2a92f017f58103684261e5b9fc7a2
Adithya Tanam <adisuresh58@gmail.com>
2026-04-09 10:17:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse first K of a Queue/reverse-first-k-of-a-queue.java
+++ Difficulty: Easy/Reverse first K of a Queue/reverse-first-k-of-a-queue.java
@@ -1,21 +1,19 @@
 class Solution {
     public Queue<Integer> reverseFirstK(Queue<Integer> q, int k) {
-        if(k<=0 || k>q.size()){
-            return q;
-        }
-        Stack<Integer> st1 = new Stack<>();
         
-        for (int i = 0; i < k; i++) {
-            st1.push(q.remove());
+        int n = q.size();
+        if(k>n||k<=0) return q;
+        RQ(q,k);
+        for(int i=0;i<n-k;i++){
+            q.add(q.poll());
         }
-        while (!st1.isEmpty()) {
-            q.add(st1.pop());
-        }
-        int rem = q.size() - k;
-        for (int i = 0; i < rem; i++) {
-            q.add(q.remove());
-        }
-
         return q;
+    }
+    private void RQ(Queue<Integer> q, int k){
+        if(k==0)
+        return;
+        int f = q.poll();
+        RQ(q,k-1);
+        q.add(f);
     }
 }
